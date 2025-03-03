029a93e5b22957be1ccc31527c3bb42e551515ba
SwedeshnaMishra <164918136+SwedeshnaMishra@users.noreply.github.com>
2025-03-03 08:55:32 +0530

    Continuous Subarrays

---
+++ LeetCode/Continuous Subarrays.cpp
@@ -0,0 +1,69 @@
+//2762. Continuous Subarrays
+
+// You are given a 0-indexed integer array nums. A subarray of nums is called continuous if:
+// Let i, i + 1, ..., j be the indices in the subarray. Then, for each pair of indices i <= i1, i2 <= j, 0 <= |nums[i1] - nums[i2]| <= 2.
+// Return the total number of continuous subarrays.
+
+// A subarray is a contiguous non-empty sequence of elements within an array.
+
+// Example 1:
+
+// Input: nums = [5,4,2,4]
+// Output: 8
+// Explanation: 
+// Continuous subarray of size 1: [5], [4], [2], [4].
+// Continuous subarray of size 2: [5,4], [4,2], [2,4].
+// Continuous subarray of size 3: [4,2,4].
+// There are no subarrys of size 4.
+// Total continuous subarrays = 4 + 3 + 1 = 8.
+// It can be shown that there are no more continuous subarrays.
+ 
+
+// Example 2:
+
+// Input: nums = [1,2,3]
+// Output: 6
+// Explanation: 
+// Continuous subarray of size 1: [1], [2], [3].
+// Continuous subarray of size 2: [1,2], [2,3].
+// Continuous subarray of size 3: [1,2,3].
+// Total continuous subarrays = 3 + 2 + 1 = 6.
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// 1 <= nums[i] <= 109
+
+class Solution {
+public:
+    long long continuousSubarrays(vector<int>& nums) {
+        int n = nums.size();
+        long long totalSubarrays = 0;
+        
+        deque<int> minDeque, maxDeque; 
+        int start = 0;
+
+        for (int end = 0; end < n; end++) {
+            while (!minDeque.empty() && nums[minDeque.back()] > nums[end]) {
+                minDeque.pop_back();
+            }
+            minDeque.push_back(end);
+
+            while (!maxDeque.empty() && nums[maxDeque.back()] < nums[end]) {
+                maxDeque.pop_back();
+            }
+            maxDeque.push_back(end);
+
+            while (nums[maxDeque.front()] - nums[minDeque.front()] > 2) {
+                if (minDeque.front() == start) minDeque.pop_front();
+                if (maxDeque.front() == start) maxDeque.pop_front();
+                start++;
+            }
+
+            totalSubarrays += (end - start + 1);
+            }
+            return totalSubarrays;
+
+    }
+};
