0eba29faae37f28ac8c44478ee0f0ead5b8545ca
SwedeshnaMishra <164918136+SwedeshnaMishra@users.noreply.github.com>
2025-03-16 10:37:05 +0530

    Add files via upload

---
+++ Geeksforgeeks/q1.cpp
@@ -0,0 +1,42 @@
+//Second Largest
+
+//Given an array of positive integers arr[], return the second largest element from the array. If the second largest element doesn't exist then return -1.
+
+// Note: The second largest element should not be equal to the largest element.
+
+// Examples:
+// Input: arr[] = [12, 35, 1, 10, 34, 1]
+// Output: 34
+// Explanation: The largest element of the array is 35 and the second largest element is 34.
+// Input: arr[] = [10, 5, 10]
+// Output: 5
+// Explanation: The largest element of the array is 10 and the second largest element is 5.
+// Input: arr[] = [10, 10, 10]
+// Output: -1
+// Explanation: The largest element of the array is 10 and the second largest element does not exist.
+// Constraints:
+// 2 ≤ arr.size() ≤ 105
+// 1 ≤ arr[i] ≤ 105
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    int getSecondLargest(vector<int> &arr) {
+        
+        int largest = INT_MIN, secondLargest = INT_MIN;
+        
+        for (int num : arr) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num < largest) {
+                secondLargest = num;
+            }
+        }
+        
+        return (secondLargest == INT_MIN) ? -1 : secondLargest;
+    }
+};
---
+++ Geeksforgeeks/q3.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+  public:
+    void reverseArray(vector<int> &arr) {
+        int i = 0, j = arr.size() - 1;
+        while(i < j){
+            swap(arr[i], arr[j]);
+            i++;
+            j--;
+        }
+    }
+};
+
