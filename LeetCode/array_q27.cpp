a1cf963c0035a5cda88b38906368c132e1d4f931
Swedeshna Mishra <164918136+SwedeshnaMishra@users.noreply.github.com>
2025-06-30 21:31:18 +0530

    Add files via upload

---
+++ LeetCode/array_q27.cpp
@@ -0,0 +1,32 @@
+//242. Valid Anagram
+
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+// Example 1:
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+
+// Example 2:
+// Input: s = "rat", t = "car"
+// Output: false
+
+// Constraints:
+// 1 <= s.length, t.length <= 5 * 104
+// s and t consist of lowercase English letter
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        unordered_map<char, int> countS;
+        unordered_map<char, int> countT;
+        for (int i = 0; i < s.length(); i++) {
+            countS[s[i]]++;
+            countT[t[i]]++;
+        }
+        return countS == countT;
+    }
+};
