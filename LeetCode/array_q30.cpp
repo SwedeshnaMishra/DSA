231fb1d882d34e8054fa55c31da88bea9448e236
Swedeshna Mishra <164918136+SwedeshnaMishra@users.noreply.github.com>
2025-07-02 20:45:44 +0530

    Add files via upload

---
+++ LeetCode/array_q30.cpp
@@ -0,0 +1,76 @@
+//36. Valid Sudoku
+
+// Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+// Each row must contain the digits 1-9 without repetition.
+// Each column must contain the digits 1-9 without repetition.
+// Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+// Note:
+// A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+// Only the filled cells need to be validated according to the mentioned rules.
+
+// Example 1:
+// Input: board = 
+// [["5","3",".",".","7",".",".",".","."]
+// ,["6",".",".","1","9","5",".",".","."]
+// ,[".","9","8",".",".",".",".","6","."]
+// ,["8",".",".",".","6",".",".",".","3"]
+// ,["4",".",".","8",".","3",".",".","1"]
+// ,["7",".",".",".","2",".",".",".","6"]
+// ,[".","6",".",".",".",".","2","8","."]
+// ,[".",".",".","4","1","9",".",".","5"]
+// ,[".",".",".",".","8",".",".","7","9"]]
+// Output: true
+
+// Example 2:
+// Input: board = 
+// [["8","3",".",".","7",".",".",".","."]
+// ,["6",".",".","1","9","5",".",".","."]
+// ,[".","9","8",".",".",".",".","6","."]
+// ,["8",".",".",".","6",".",".",".","3"]
+// ,["4",".",".","8",".","3",".",".","1"]
+// ,["7",".",".",".","2",".",".",".","6"]
+// ,[".","6",".",".",".",".","2","8","."]
+// ,[".",".",".","4","1","9",".",".","5"]
+// ,[".",".",".",".","8",".",".","7","9"]]
+// Output: false
+// Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+
+// Constraints:
+// board.length == 9
+// board[i].length == 9
+// board[i][j] is a digit 1-9 or '.'.
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        for (int row = 0; row < 9; row++) {
+            unordered_set<char> seen;
+            for (int i = 0; i < 9; i++) {
+                if (board[row][i] == '.') continue;
+                if (seen.count(board[row][i])) return false;
+                seen.insert(board[row][i]);
+            }
+        }
+        for (int col = 0; col < 9; col++) {
+            unordered_set<char> seen;
+            for (int i = 0; i < 9; i++) {
+                if (board[i][col] == '.') continue;
+                if (seen.count(board[i][col])) return false;
+                seen.insert(board[i][col]);
+            }
+        }
+        for (int square = 0; square < 9; square++) {
+            unordered_set<char> seen;
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    int row = (square/3) * 3 + i;
+                    int col = (square%3) * 3 + j;
+                if (board[row][col] == '.') continue;
+                if (seen.count(board[row][col])) return false;
+                seen.insert(board[row][col]);
+                }
+            }
+        }
+        return true;
+    }
+};
